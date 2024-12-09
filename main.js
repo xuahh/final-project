5d3801dc49c941202012e95e2ac0fc8e1659845b
xuahh <bhq061@myy.haaga-helia.fi>
2024-12-09 16:58:15 +0200

    add files

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
 function confetti () {
-    console.log('joy')
     window.location.href = 'https://publuu.com/flip-book/743887/1648965'
-    // Pass in the id of an element
-    let confetti = new Confetti('demo');
-
-    // Edit given parameters
-    confetti.setCount(75);
-    confetti.setSize(1);
-    confetti.setPower(25);
-    confetti.setFade(false);
-    confetti.destroyTarget(true);
 }
